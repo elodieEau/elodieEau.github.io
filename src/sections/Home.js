dc5f472ca553e7299e3febb1ca67a4b42fa86fc3
Charles Zimmerlin <charles.zimmerlin@gmail.com>
2022-10-20 18:34:52 +0200

    Homepage ended

--- src/sections/Home.js
+++ src/sections/Home.js
@@ -1,4 +1,7 @@
 import React from "react";
+import circlesTop from "../images/circlesTop.png";
+import circlesLeft from "../images/circlesLeft.png";
+import eloFace from "../images/eloFace.png";
 //import Typewriter from 'typewriter-effect';
 
 import '../styles/Home.css';
@@ -6,6 +9,7 @@ import '../styles/Home.css';
 
 const Home = React.forwardRef((props, ref) => (
     <div ref={ref} className="homePage" id="home" >
+        <img src={circlesTop} className="circlesTop" alt="circles"/>
         <div className="homeContainerTop">
             <div className="hiText">
                 <div>
@@ -16,7 +20,23 @@ const Home = React.forwardRef((props, ref) => (
                     <span className="titleSecond">a future engineer</span>
                 </div>
             </div>
-        </div>
+            </div>
+            <div className="homeContainerBottom">
+                <div className="profilBox">
+                    <img src={circlesLeft} className="circlesLeft" alt="circles"/>
+                    <div className="titleHome">
+                        <div className="homeName">
+                            <span className="nameP">Elodie</span><br/>
+                            <span className="nameN">Aqua</span>
+                        </div>
+                        <div className="homeDesc">Mechanical engineering student, designer and 
+                        nature and sports lover
+                        </div>
+                    </div>
+                    <img src={eloFace} className="eloFace" alt="face"/>
+                </div>
+            </div>
+
     </div >
 ));
 
