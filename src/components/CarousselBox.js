c476a7300fe8a2b07dfb34be98ea7a723e95098f
Charles Zimmerlin <charles.zimmerlin@gmail.com>
2022-10-22 15:08:39 +0200

    Responsive and caroussel mobile

--- src/components/CarousselBox.js
+++ src/components/CarousselBox.js
@@ -1,9 +1,13 @@
 import React, { useEffect, useState } from "react";
-import leftArrow from "../images/leftArrow.png";
-import rightArrow from "../images/rightArrow.png";
+import { Fade } from "react-awesome-reveal";
+
 import Quote from "./Quote";
 import Title from "./Title";
-import { Fade } from "react-awesome-reveal";
+
+
+import leftArrow from "../images/leftArrow.png";
+import rightArrow from "../images/rightArrow.png";
+import line from "../images/line.png";
 
 function getWindowSize() {
     const { innerWidth, innerHeight } = window;
@@ -33,76 +37,112 @@ export default function CarousselBox(props) {
     const invert = (props.invert && windowSize.innerWidth > 991) || false;
 
     return (
-        <div className="caroussel">
-            <div className="bigArrowBox" onClick={() => {
-                if (currentIndex > 0) {
-                    setCurrentIndex((currentIndex - 1) % length);
-                } else {
-                    setCurrentIndex(length - 1);
-                }
-            }} >
-                <div className="smallArrow">
-                    <Fade direction="left" delay={50} duration={700} triggerOnce={true}>
-                        <img src={leftArrow} alt="leftArrow" className="arrow" />
-                    </Fade>
+        <>
+            <div className="caroussel">
+                <div className="bigArrowBox" onClick={() => {
+                    if (currentIndex > 0) {
+                        setCurrentIndex((currentIndex - 1) % length);
+                    } else {
+                        setCurrentIndex(length - 1);
+                    }
+                }} >
+                    <div className="smallArrow">
+                        <Fade direction="left" delay={50} duration={700} triggerOnce={true}>
+                            <img src={leftArrow} alt="leftArrow" className="arrow" />
+                        </Fade>
+                    </div>
                 </div>
-            </div>
-            <div className="midBox">
-                {
-                    invert ?
-                        <>
-                            <div className="rightBox">
-                                <Fade direction="up" delay={50} duration={700} triggerOnce={true}>
-                                    <img src={image} alt="imag right" className="rightImage" />
-                                </Fade>
-                            </div>
-                            <div className="leftBox">
-                                <div className="topBox">
-                                    <Fade direction="up" delay={150} duration={700} triggerOnce={true}>
-                                        <Title top={top} bot={bot} color="#474747" mgLeft="0" align="right" />
-                                    </Fade>
-                                </div>
-                                <div className="botBox">
-                                    <Fade direction="up" delay={250} duration={700} triggerOnce={true}>
-                                        <Quote text={text} mg="0" />
-                                    </Fade>
-                                </div>
-                            </div>
-                        </>
-                        :
-                        <>
-                            <div className="leftBox">
-                                <div className="topBox">
+                <div className="midBox">
+                    {
+                        invert ?
+                            <>
+                                <div className="rightBox">
                                     <Fade direction="up" delay={50} duration={700} triggerOnce={true}>
-                                        <Title top={top} bot={bot} color="#474747" mgLeft="0" />
+                                        <img src={image} alt="imag right" className="rightImage" />
                                     </Fade>
                                 </div>
-                                <div className="botBox">
-                                    <Fade direction="up" delay={150} duration={700} triggerOnce={true}>
-                                        <Quote text={text} mg="0" />
+                                <div className="leftBox">
+                                    <div className="topBox">
+                                        <Fade direction="up" delay={150} duration={700} triggerOnce={true}>
+                                            <Title top={top} bot={bot} color="#474747" mgLeft="0" align="right" />
+                                        </Fade>
+                                    </div>
+                                    <div className="botBox">
+                                        <Fade direction="up" delay={250} duration={700} triggerOnce={true}>
+                                            <Quote text={text} mg="0" />
+                                        </Fade>
+                                    </div>
+                                </div>
+                            </>
+                            :
+                            <>
+                                <div className="leftBox">
+                                    <div className="topBox">
+                                        <Fade direction="up" delay={50} duration={700} triggerOnce={true}>
+                                            <Title top={top} bot={bot} color="#474747" mgLeft="0" />
+                                        </Fade>
+                                    </div>
+                                    <div className="botBox">
+                                        <Fade direction="up" delay={150} duration={700} triggerOnce={true}>
+                                            <Quote text={text} mg="0" />
+                                        </Fade>
+                                    </div>
+                                </div>
+
+                                <div className="rightBox">
+                                    <Fade direction="up" delay={250} duration={700} triggerOnce={true}>
+                                        <img src={image} alt="imag right" className="rightImage" />
                                     </Fade>
                                 </div>
-                            </div>
+                            </>
 
-                            <div className="rightBox">
-                                <Fade direction="up" delay={250} duration={700} triggerOnce={true}>
-                                    <img src={image} alt="imag right" className="rightImage" />
-                                </Fade>
-                            </div>
-                        </>
-
-                }
-            </div>
-            <div className="bigArrowBox" onClick={() => {
-                setCurrentIndex((currentIndex + 1) % length)
-            }}>
-                <div className="smallArrow">
-                    <Fade direction="right" delay={50} duration={700} triggerOnce={true}>
-                        <img src={rightArrow} alt="rightArrow" className="arrow" />
+                    }
+                </div>
+                <div className="bigArrowBox" onClick={() => {
+                    setCurrentIndex((currentIndex + 1) % length)
+                }}>
+                    <div className="smallArrow">
+                        <Fade direction="right" delay={50} duration={700} triggerOnce={true}>
+                            <img src={rightArrow} alt="rightArrow" className="arrow" />
+                        </Fade>
+                    </div>
+                </div>
+            </div >
+            <div className="carousselMobile">
+                <div className="imageM">
+                    <Fade direction="up" delay={50} duration={700} triggerOnce={true}>
+                        <img src={image} alt="imag right" className="rightImageM" />
                     </Fade>
                 </div>
+                <div className="contentM">
+                    <Fade direction="up" delay={150} duration={700} triggerOnce={true}>
+                        <Title top={top} bot={bot} color="#474747" mgLeft="50px" align="left" />
+                    </Fade>
+                    <Fade direction="up" delay={250} duration={700} triggerOnce={true}>
+                        <Quote text={text} mg="0" />
+                    </Fade>
+                </div>
+                <div className="btnArrows">
+                    <Fade direction="left" delay={350} duration={700} triggerOnce={true} >
+                        <img src={leftArrow} alt="leftArrow" className="arrowM" onClick={() => {
+                            if (currentIndex > 0) {
+                                setCurrentIndex((currentIndex - 1) % length);
+                            } else {
+                                setCurrentIndex(length - 1);
+                            }
+                        }} />
+                    </Fade>
+                    <Fade direction="left" delay={350} duration={700} triggerOnce={true}>
+                        <img src={rightArrow} alt="rightArrow" className="arrowM" onClick={() => {
+                            setCurrentIndex((currentIndex + 1) % length)
+                        }} />
+                    </Fade>
+                </div>
+                <div className="lineContainer">
+                    <img src={line} alt="rightArrow" className="line" />
+                </div>
             </div>
-        </div >
+        </>
 
     );
 }
